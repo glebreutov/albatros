c63d04e374db92e8bb4253fe7b1948492d9b2c83
glebreutov <glebr@algotradinghub.com>
2017-10-13 19:38:43 +0300

    message structure test has added

--- tests/test-exec-messages.js
+++ tests/test-exec-messages.js
@@ -1,9 +1,18 @@
-function testAny(any){
+function testAny (any) {
   return 'ack' in any && typeof any.ack === 'boolean'
 }
 
-function testOrderStatus(os){
+function testOrderStatus (os) {
   return testAny(os) && 'id' in os && 'exch' in os && 'pair' in os
 }
 
-console.log(testAny({ack: 1}))
+function testOrderStatus (os) {
+  return testAny(os) && 'remains' in os && typeof os.remains === 'number'
+}
+
+console.log(testAny({ack: true}))
+console.log(testOrderStatus({ remains: 0.01,
+  resp:
+    { ack: true,
+      payload: { success: true, message: '', result: [Object] } },
+  ack: true }))
